c8a0fce378c7eddd9a3bee7c5f4bcc16e7e122cf
Renan <renanalmeida201612@gmail.com>
2023-12-10 23:53:43 -0300

    feat: planos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,12 @@ import PartyupBusiness from "../assets/images/partyup-business.png";
 import { useNavigate } from "react-router";
 import Logo from "../assets/logo.svg";
 import Benefits from "../components/Benefits";
+import { CiCirclePlus } from "react-icons/ci";
 
 export default function Home() {
   const navigate = useNavigate();
+  const [viewBenefits, setViewBenefits] = useState(false);
+  const [viewBenefitsAnual, setViewBenefitsAnual] = useState(false);
   return (
     <div className="min-h-screen bg-[#2A0127]">
       <Navbar />
@@ -56,29 +59,16 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-center text-purple-200 text-border-pink-400 text-border-size-[16px]">
         Conheça nossos planos
       </h1>
-      <div className="py-6">
-        <div className="flex items-center text-white bg-[#61075B] w-[40%] rounded-r-[3rem] my-12 px-12 pb-4">
+      <div className="relative flex flex-col items-start">
+        <div
+          onClick={() =>
+            (location.href = "https://buy.stripe.com/4gw7u8cdc3AEdR6289")
+          }
+          className="flex items-center text-white cursor-pointer bg-[#61075B] w-[45%] rounded-r-[3rem] mt-12 mb-4 px-16 py-4 hover:bg-[#6b2567]">
           <img src={Logo} width={150} className="-mb-5" />
           <h1 className="ml-36 text-4xl font-bold text-center text-purple-100 text-border-pink-400 text-border-size-[16px]">
             Plano <br /> Mensal
           </h1>
-          <ul className="w-[20rem] m-auto text-xs flex flex-col gap-4 hidden">
-            <li>
-              - Acesso completo ao Dashboard, proporcionando uma visão detalhada
-              e abrangente do desempenho de seus eventos.
-            </li>
-            <li>
-              - Suporte ao cliente dedicado, com assistência por e-mail,
-              garantindo respostas prioritárias para resolver eventuais questões
-              ou dúvidas.
-            </li>
-            <li>
-              - Atualizações regulares de recursos, mantendo a plataforma
-              alinhada às últimas tendências e demandas do mercado. Filtro de
-              análise de métricas conforme as necessidades específicas de seus
-              eventos.
-            </li>
-          </ul>
           <h1 className="ml-auto text-6xl font-bold text-purple-100">
             <span className="mr-1 text-2xl">R$</span>
             <span className="text-purple-200 text-border-pink-600 text-border-size-[8px]">
@@ -87,9 +77,61 @@ export default function Home() {
             <span className="ml-1 text-xl align-top">,99</span>
           </h1>
         </div>
+        <button
+          onClick={() => setViewBenefits(!viewBenefits)}
+          className="flex items-center gap-2 text-xl font-medium text-white">
+          <h1 className="ml-6">Mostrar benefícios</h1>
+          <CiCirclePlus size={30} />
+        </button>
+        <ul
+          className={`w-[50rem] ml-10 mt-4 text-lg flex flex-col gap-4 font-['Roboto'] text-white list-disc ${
+            !viewBenefits && "invisible"
+          }`}>
+          <li>
+            Acesso completo ao Dashboard, proporcionando uma visão detalhada e
+            abrangente do desempenho de seus eventos.
+          </li>
+          <li>
+            Suporte ao cliente dedicado, com assistência por e-mail, garantindo
+            respostas prioritárias para resolver eventuais questões ou dúvidas.
+          </li>
+          <li>
+            Atualizações regulares de recursos, mantendo a plataforma alinhada
+            às últimas tendências e demandas do mercado. Filtro de análise de
+            métricas conforme as necessidades específicas de seus eventos.
+          </li>
+        </ul>
       </div>
-      <div className="flex justify-end py-6">
-        <div className="flex items-center flex-row-reverse text-white bg-[#61075B] w-[40%] rounded-l-[3rem] px-12 pb-4">
+      <div className="flex flex-col items-end py-6 -mt-[30rem]">
+        <ul
+          className={`w-[50rem] ml-10 text-lg flex flex-col gap-4 text-white list-disc mb-6 font-['Roboto'] ${
+            !viewBenefitsAnual && "invisible"
+          }`}>
+          <li>
+            Acesso completo ao Dashboard, proporcionando uma visão detalhada e
+            abrangente do desempenho de seus eventos.
+          </li>
+          <li>
+            Suporte ao cliente dedicado, com assistência por e-mail, garantindo
+            respostas prioritárias para resolver eventuais questões ou dúvidas.
+          </li>
+          <li>
+            Atualizações regulares de recursos, mantendo a plataforma alinhada
+            às últimas tendências e demandas do mercado. Filtro de análise de
+            métricas conforme as necessidades específicas de seus eventos.
+          </li>
+        </ul>
+        <button
+          onClick={() => setViewBenefitsAnual(!viewBenefitsAnual)}
+          className="z-10 flex items-center gap-2 mr-4 text-xl font-medium text-white">
+          <h1>Mostrar benefícios</h1>
+          <CiCirclePlus size={30} />
+        </button>
+        <div
+          onClick={() =>
+            (location.href = "https://buy.stripe.com/14k3dS2CC1sw7sI3ce")
+          }
+          className="flex items-center flex-row-reverse cursor-pointer text-white bg-[#61075B] w-[45%] mt-4 rounded-l-[3rem] px-16 py-4 hover:bg-[#6b2567]">
           <img src={Logo} width={150} className="-mb-5" />
           <h1 className="mr-36 text-4xl font-bold text-center text-purple-100 text-border-pink-400 text-border-size-[16px]">
             Plano <br /> Anual
